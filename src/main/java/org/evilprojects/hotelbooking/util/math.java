1733b71e281ff841d0b3a07906f22c03b77347f1
NotEvil <notevil036@gmail.com>
2024-02-11 17:57:51 +0200

    Discount formula fix

--- src/main/java/org/evilprojects/hotelbooking/util/math.java
+++ src/main/java/org/evilprojects/hotelbooking/util/math.java
@@ -2,6 +2,6 @@ package org.evilprojects.hotelbooking.util;
 
 public class math {
     public static double getDiscount(double price, int discount){
-        return discount < 0 || discount > 100 ? price : price * (100 - discount);
+        return discount < 0 || discount > 100 ? price : price * (1 - (double)discount / 100);
     }
 }
